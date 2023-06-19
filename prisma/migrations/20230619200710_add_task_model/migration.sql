82a2ea4f400d4b4d1fa30e3c0ca7a9f1e942a4c9
Eric Oliveira <oiericoi@hotmail.co>
2023-06-19 17:27:12 -0300

    feat: generate task model migration

---
+++ prisma/migrations/20230619200710_add_task_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "finished" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Task_id_key" ON "Task"("id");
