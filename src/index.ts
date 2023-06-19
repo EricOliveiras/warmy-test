cdab77b600a6e2f773b95991923202111a6d7272
Eric Oliveira <oiericoi@hotmail.co>
2023-06-19 17:33:31 -0300

    feat: add project index file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import 'express-async-errors';
+import { app } from './config/server';
+import { port } from './config/vars';
+
+app.listen(port, () => {
+  console.log(`server listening at port: ${port}`);
+});
