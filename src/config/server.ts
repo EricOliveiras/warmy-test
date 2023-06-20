dbc904de96f3f2f03ac3586301dee51a5370c944
Eric Oliveira <oiericoi@hotmail.co>
2023-06-20 09:23:24 -0300

    feat: add error middleware in server config

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import { errorMiddleware } from '../middleware/errorMiddleware';
 
 export const app = express();
 
 app.use(express.json());
 
 app.use(cors());
+
+app.use(errorMiddleware);
