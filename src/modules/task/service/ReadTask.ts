051036773ed8be8c4d41354623b6eb0290b5bc43
Eric Oliveira <oiericoi@hotmail.co>
2023-06-20 09:29:52 -0300

    feat: add read task service

---
+++ src/modules/task/service/ReadTask.ts
@@ -0,0 +1,16 @@
+import { HttpException } from '../../../errors/HttpException';
+import { TaskRepoitory } from '../repository/TaskRepository';
+
+export class ReadTask {
+  constructor(private taskRepository: TaskRepoitory) {}
+
+  public async execute(id: string) {
+    const task = await this.taskRepository.read(id);
+
+    if (!task) {
+      throw new HttpException(404, 'Task not found.');
+    }
+
+    return task;
+  }
+}
