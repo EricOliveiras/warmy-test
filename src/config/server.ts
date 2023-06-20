11453625f9fee487119a649aa6c3cbf6ada0fae6
Eric Oliveira <oiericoi@hotmail.co>
2023-06-20 10:34:35 -0300

    feat: add routes

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import { errorMiddleware } from '../middleware/errorMiddleware';
+import { router } from '../routes';
 
 export const app = express();
 
 app.use(express.json());
 
 app.use(cors());
+
+app.use(router);
 
 app.use(errorMiddleware);
